bed748c6e2a678c3568d629afa92434d3d557c9d
jmlb23 <contadoinstituto@gmail.com>
2015-07-02 21:07:26 +0200

    php non ten constructores por defecto XDD

--- oop/herencia.php
+++ oop/herencia.php
@@ -9,7 +9,7 @@
 //o modelo de obxetos e clases é parecido o de c++
 //non hai unha clase "object" da que todas as clases hernden
 /**
-@link http://stackoverflow.com/questions/7909073/why-php-has-no-default-constructor
+*@link http://stackoverflow.com/questions/7909073/why-php-has-no-default-constructor
 */
 class persoa{
 	public function __construct($nome){
