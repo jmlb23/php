ebdc34dfebb246b6132fe22b81daeec72d66565f
jmlb23 <jesus@debian>
2015-07-02 12:32:26 +0200

    faltaba o exemplo

--- names/erro_name.php
+++ names/erro_name.php
@@ -1,5 +1,8 @@
 <!-- a declaracion dun namespace ten que ser obrigatoriamente a primeira excepto 
-se hai un declare -->
+se hai un declare non pode ir nin html antes
+exemplo
+-->
+<html>
 <?php
 
 namespace erro_name;
