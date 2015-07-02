a4bee17722e720db7ad976a6531bca04d8e14dab
jesus <jesus@debian>
2015-07-02 12:22:08 +0200

    exemplificacion dos tre tipos de names en php

--- names/dinamico2.php
+++ names/dinamico2.php
@@ -53,3 +53,31 @@ echo "<br/>";
 echo constant("dinamico\\CONS");
 echo "<br/>";
 echo constant("dinamico\\proba::V");
+echo "<hr/>proba coa resolucion de nomes coas clases<hr>";
+//a resolucion de nomes como me parecia no documento arc_name2.php so funciona con funcions e constantes
+//noutras palabras se neste namespace facemos unha chamada a strlen e non esta neste declarada outra funcion php chama
+//a do espazo de nomes global e nas variables e constanstes tamen
+//pero as clases non funciona da mesma maneira e hai que indicalo de maneira explicita 
+//co backslash para indicar que nos referimos univocamente
+//a clase global
+
+
+/*******************************************************************************************************************/
+//por facer un simil os namespaces funcionan de maneira moi similar a estructura de directorios nun GNU/linux
+//se estamos nun directorio e facemos uso dunha ruta relativa vai buscar ese elemento no directorio actual o igual que fai php
+// se usamos unha ruta absolta dende a raiz vai /etc vai buscas na raiz ese elemento o igual que en php new \Exception busca no
+//espazo de nomes global
+//se e so qualified e o mesmo que facer se estamos en ~ e facemos cd Escritorio/kk mira nese directorio por ese elemento no actual
+//e se é unqualified sen nengun slash php para as clases mira so no namespace actual como se estamos no home e facemos cd kk mira no
+//actual
+//en php os 3 tipos de nomenclaturas son () exemplificado en name2.php
+//1 fully qualified \StdClass 
+//2 qualified namespace\elemento
+//3 unqualified
+/*******************************************************************************************************************/
+$h = new \StdClass();//chamamos explicitamente a global
+//recordar que so php é casensitive nas variables
+//echo Cons;
+var_dump($h);
+$h = new stdClass(); //intentamos chamar a global con unqualified pero da fatal error porque non o atopa neste scope
+
---
+++ names/name2.php
@@ -0,0 +1,31 @@
+<?php
+namespace h{
+	function x(){
+		echo "hola dende ".__METHOD__."<br/>";
+	}
+	class stdClass{
+		function __construct(){
+			echo "hola dende ".__CLASS__."<br/>";
+		}
+	}
+	//resolve o propio porque hai que explicitar un full qualified para referirnos o elemento do espazo de nomes global
+	$v = new stdClass();
+}
+
+namespace{
+	function x(){
+		echo "adeus"."<br/>";
+	}
+	echo \h\x();
+	echo h\x();
+	echo x();
+
+	$a = new \h\stdClass();
+	$b = new h\stdClass();//resolve da seguinte forma estamo no global xa que é o que non ten nome e o estarmos no mesmo ficheiro
+	//é como buscar un elemento no dir actual
+	var_dump($c = new stdClass());//resolve ben porque estamos no global e non fai falla facer un full qualified
+}
+namespace v{
+	//falla porque intenta cargar unha clase con ese nome neste espazo de nomes
+	$h = new stdClass();
+}
